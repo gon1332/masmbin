78646e91b7374e5f1ff4fa48b37f8a0880499817
Ioannis Konstadelias <ikonstadel@gmail.com>
2015-07-14 15:49:23 +0300

    Change a bit the code organization for clarity.

--- src/main.c
+++ src/main.c
@@ -5,19 +5,31 @@ void yyparse(void);
 
 int main(int argc, char *argv[])
 {
-    extern FILE *yyin;
-
-    argv++, argc--;
-    if (3 == argc) {
-        if (NULL == (yyin = fopen(argv[0], "r"))) {
-            perror("masmbin: error");
-            exit(EXIT_FAILURE);
-        }
-    } else {
+    /* Check for argument validity.
+     * */
+    if (argc != 3) {
         printf("Usage: masmbin <in_file> <out_file>\n");
         exit(EXIT_FAILURE);
     }
 
+    /* Open 1st argument for read.
+     * */
+    extern FILE *yyin;
+    if (NULL == (yyin = fopen(argv[1], "r"))) {
+        perror("masmbin: error");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Open 2nd argument for write.
+     * */
+    FILE *fout;
+    if (NULL == (fout = fopen(argv[2], "w"))) {
+        perror("masmbin: error");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Parse the input file.
+     * */
     yyparse();
 
     return EXIT_SUCCESS;
