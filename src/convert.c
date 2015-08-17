12a7e89a47ce324c024337f4156cc26f5e11f931
Ioannis Konstadelias <ikonstadel@gmail.com>
2015-08-17 13:22:53 +0300

    Change tab-formatting to space-formatting.

--- src/convert.c
+++ src/convert.c
@@ -60,19 +60,19 @@ int convert_hex_bin_oct_to_dec(const char *input)
 
 char *baseconv(unsigned int num, int base)
 {
-	static char retbuf[33];
-	char *p;
+    static char retbuf[33];
+    char *p;
 
-	if (base < 2 || base > 16)
-		return NULL;
+    if (base < 2 || base > 16)
+        return NULL;
 
-	p = &retbuf[sizeof(retbuf)-1];
-	*p = '\0';
+    p = &retbuf[sizeof(retbuf)-1];
+    *p = '\0';
 
-	do {
-		*--p = "0123456789abcdef"[num % base];
-		num /= base;
-	} while (num != 0);
+    do {
+        *--p = "0123456789abcdef"[num % base];
+        num /= base;
+    } while (num != 0);
 
     // Sign-extent positive base-2 numbers
     if (base == 2 && p != retbuf) {
@@ -81,5 +81,5 @@ char *baseconv(unsigned int num, int base)
         } while (p != retbuf);
     }
 
-	return p;
+    return p;
 }
